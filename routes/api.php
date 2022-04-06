e309077e807f60dd1e52e030073f374e6787bfad
loridev <79363993+loridev@users.noreply.github.com>
2022-04-06 12:10:34 +0200

    Fix artisan + setup rankings

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/v1/auth')->group(function () {
-    Route::post('/register', [UserController::class, 'register']);
-    Route::post('/login', [UserController::class, 'login']);
+Route::prefix('/v1')->group(function () {
+    Route::prefix('/auth')->group(function () {
+        Route::post('/register', [UserController::class, 'register']);
+        Route::post('/login', [UserController::class, 'login']);
 
-    Route::middleware('auth:api')->group(function () {
-        Route::post('current', [UserController::class, 'currentUser']);
-        Route::post('logout', [UserController::class, 'logout']);
+        Route::middleware('auth:api')->group(function () {
+            Route::post('current', [UserController::class, 'currentUser']);
+            Route::post('logout', [UserController::class, 'logout']);
+        });
+    });
+
+    Route::prefix('/rankings')->group(function () {
+        Route::prefix('/single')->group(function () {
+            Route::get('/', [[RankingController::class, 'getIndiv']])->middleware('auth:api');
+            Route::get('/current', [RankingController::class, 'getIndiv']);
+        });
+        Route::prefix('/multi')->group(function () {
+            Route::get('/', [[RankingController::class, 'getMulti']])->middleware('auth:api');
+            Route::get('/current', [RankingController::class, 'getMulti']);
+        });
     });
 });
+
+
 
