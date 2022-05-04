3f167873e7f5552745f08c203c4439dae714e22d
albacandelario <alba.candelario@gracia.lasalle.cat>
2022-05-04 16:54:29 +0200

    video responsive bug fixed

--- resources/js/UI/Video/Video.jsx
+++ resources/js/UI/Video/Video.jsx
@@ -1,12 +1,21 @@
+import clases from "./styles/Video.module.css";
+
 function Video(props) {
 
     return (
-        <video autoPlay="autoplay" muted="muted" loop="loop" className={props.className}>
+        <>
+            <video autoPlay="autoplay" muted="muted" loop="loop" className={`${props.className} ${clases.desktop}`}>
 
-            <source src="/videos/mapacheMobile.mp4" type="video/mp4" media="all and (max-width: 648px)"></source>
-            <source src="/videos/mapache.mp4" type="video/mp4"></source>
-            {props.children}
-        </video>
+                <source src="/videos/mapache.mp4" type="video/mp4"></source>
+                {props.children}
+                </video>
+
+            <video autoPlay="autoplay" muted="muted" loop="loop" className={`${props.className} ${clases.mobile}`}>
+
+                <source src="/videos/mapacheMobile.mp4" type="video/mp4"></source>
+                {props.children}
+            </video>
+        </>
     );
 }
 
