71532088c29374f9420774bc2d4cefc0ba9f4220
albacandelario <alba.candelario@gracia.lasalle.cat>
2022-05-05 17:53:33 +0200

    favIcon done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CARLOS EL BOMBAS</title>
+    <link rel="icon" href="/images/logomobile.svg">
 </head>
 <body>
     <div id="root"></div>
